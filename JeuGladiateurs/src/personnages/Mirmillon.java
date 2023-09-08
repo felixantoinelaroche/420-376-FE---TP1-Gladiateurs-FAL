04ade0453270810f0e7a6d681ee3dfc3d2498572
202231724 <202231724@PO-21496>
2023-09-08 15:05:14 -0400

    Classes Mirmillon et Rétiaire créées

---
+++ JeuGladiateurs/src/personnages/Mirmillon.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package personnages;
+
+/**
+ *
+ * @author Félix-Antoine Laroche
+ */
+public class Mirmillon {
+    //**************************************************************************
+    //<editor-fold desc="ATTRIBUTS">
+
+    //</editor-fold>
+
+    //**************************************************************************
+    //<editor-fold defaultstate="collapsed" desc="CONSTRUCTEURS ET INITIALISATION">
+
+    //</editor-fold>
+
+    //**************************************************************************
+    //<editor-fold desc="MÉTHODES">
+
+    //</editor-fold>
+
+    //**************************************************************************
+    //<editor-fold defaultstate="collapsed" desc="GETTERS ET SETTERS">
+
+    //</editor-fold>
+}
---
+++ JeuGladiateurs/src/personnages/Rétiaire.java
@@ -0,0 +1,23 @@
+package personnages;
+
+public class Rétiaire {
+    //**************************************************************************
+    //<editor-fold desc="ATTRIBUTS">
+    
+    //</editor-fold>
+
+    //**************************************************************************
+    //<editor-fold defaultstate="collapsed" desc="CONSTRUCTEURS ET INITIALISATION">
+
+    //</editor-fold>
+
+    //**************************************************************************
+    //<editor-fold desc="MÉTHODES">
+
+    //</editor-fold>
+
+    //**************************************************************************
+    //<editor-fold defaultstate="collapsed" desc="GETTERS ET SETTERS">
+
+    //</editor-fold>
+}
